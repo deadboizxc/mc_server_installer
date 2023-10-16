f8c4c1f9b71d8c66c1146fe3f39a768db3a58391
deadboizxc <deadboi.zxc@gmail.com>
2023-10-16 21:20:43 +0200

    update lang in scripts

--- server-install.sh
+++ server-install.sh
@@ -2,101 +2,98 @@
 
 SERVER_DIR=mc_server
 
-# Проверка на существование папки mc_server
+# Перевірка на існування теки mc_server
 if [ ! -d "$HOME/$SERVER_DIR" ]; then
     mkdir "$HOME/$SERVER_DIR"
 fi
 
-# Функция для вывода текста цветом
+# Функція для виведення тексту кольором
 print_color_text() {
     local text="$1"
     local color="$2"
     echo -e "${color}${text}\e[0m"
 }
 
-# Обновление системы
+# Оновлення системи
 clear
-print_color_text "Обновление системы..." "\e[36m"
+print_color_text "Оновлення системи..." "\e[36m"
 apt update && apt upgrade -y
 sleep 1
 clear
 
-# Установка OpenJDK и wget
-print_color_text "Установка OpenJDK и wget..." "\e[36m"
+# Встановлення OpenJDK та wget
+print_color_text "Встановлення OpenJDK та wget..." "\e[36m"
 apt install openjdk-17-jdk wget jq nano -y
 sleep 1
 clear
 
-# Запрос пользователя о скачивании ngrok
-echo -n -e "\e[36mХотите скачать и установить ngrok? (yes/no/y/n): \e[0m" && read download_ngrok
+# Запит користувача щодо завантаження ngrok
+echo -n -e "\e[36mХочете завантажити та встановити ngrok? (yes/no/y/n): \e[0m" && read download_ngrok
 
-# Проверка ответа пользователя и установка ngrok при необходимости
+# Перевірка відповіді користувача та встановлення ngrok за необхідності
 if [ "$download_ngrok" == "yes" ] || [ "$download_ngrok" == "y" ]; then
-    # Проверка на существование ссылки на ngrok
+    # Перевірка на існування посилання на ngrok
     if ! wget --spider https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz 2>/dev/null; then
-        print_color_text "Ссылка на ngrok не доступна." "\e[31m"
+        print_color_text "Посилання на ngrok недоступне." "\e[31m"
         exit 1
     fi
-    # Если ссылка на ngrok доступна, то можно скачать и установить ngrok
-    print_color_text "Скачивание и установка ngrok..." "\e[36m"
+    # Якщо посилання на ngrok доступне, то можна завантажити та встановити ngrok
+    print_color_text "Завантаження та встановлення ngrok..." "\e[36m"
     wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz
     tar xvzf ngrok-v3-stable-linux-arm64.tgz -C /usr/local/bin
 fi
 sleep 1
 clear
 
-
-# Загрузка JSON-файла с информацией о версиях Minecraft
-print_color_text "Загрузка информации о версиях Minecraft..." "\e[36m"
+# Завантаження JSON-файлу з інформацією про версії Minecraft
+print_color_text "Завантаження інформації про версії Minecraft..." "\e[36m"
 version_info=$(curl -s https://gist.githubusercontent.com/osipxd/6119732e30059241c2192c4a8d2218d9/raw/8999ab98f5779901780c3ef7a3f8b7b86a7e4281/paper-versions.json)
 
-# Проверка на наличие ошибок при загрузке JSON
+# Перевірка на наявність помилок при завантаженні JSON
 if [ $? -ne 0 ]; then
-    print_color_text "Ошибка при загрузке информации о версиях Minecraft." "\e[31m"
+    print_color_text "Помилка при завантаженні інформації про версії Minecraft." "\e[31m"
     exit 1
 fi
 sleep 1
 
-# Вывод доступных версий Minecraft
-print_color_text "Доступные версии Minecraft:" "\e[36m" # Используйте "\e[0m" для желтого цвета
+# Виведення доступних версій Minecraft
+print_color_text "Доступні версії Minecraft:" "\e[36m" # Використовуйте "\e[0m" для жовтого кольору
 jq -r '.versions | keys[]' <<< "$version_info" | while IFS= read -r version; do
   print_color_text "$version" "\e[32m"
 done
 
-# Запрос пользователя для выбора версии
-echo -n -e "\e[36mВыберите версию Minecraft для установки: \e[91m" && read minecraft_version
+# Запит користувача для вибору версії
+echo -n -e "\e[36mВиберіть версію Minecraft для встановлення: \e[91m" && read minecraft_version
 
-# Проверка, существует ли выбранная версия в списке доступных версий
+# Перевірка, чи існує обрана версія в списку доступних версій
 if ! jq -r --arg ver "$minecraft_version" '.versions | keys[] | select(. == $ver)' <<< "$version_info" >/dev/null; then
-    print_color_text "Выбранная версия не найдена." "\e[31m"
+    print_color_text "Обрана версія не знайдена." "\e[31m"
     exit 1
 fi
 
-
-
-# URL для выбранной версии Minecraft
+# URL для обраної версії Minecraft
 minecraft_url=$(jq -r --arg ver "$minecraft_version" '.versions[$ver]' <<< "$version_info")
-# Проверка наличия URL
+
+# Перевірка наявності URL
 if [ -z "$minecraft_url" ]; then
-    print_color_text "Выбранная версия не найдена." "\e[31m"
+    print_color_text "Обрана версія не знайдена." "\e[31m"
     exit 1
 fi
 sleep 1
 clear
 
-
-# Загрузка выбранной версии Minecraft
-print_color_text "Загрузка Minecraft версии $minecraft_version..." "\e[36m"
+# Завантаження обраної версії Minecraft
+print_color_text "Завантаження Minecraft версії $minecraft_version..." "\e[36m"
 if ! wget -O $HOME/$SERVER_DIR/server.jar "$minecraft_url"; then
-    print_color_text "Ошибка при загрузке сервера Minecraft." "\e[31m"
+    print_color_text "Помилка при завантаженні сервера Minecraft." "\e[31m"
     exit 1
 fi
 
-# Создание файла eula.txt
+# Створення файлу eula.txt
 echo "eula=true" > $HOME/$SERVER_DIR/eula.txt
 sleep 1
 clear
 
-# Инструкции для запуска сервера
-print_color_text "Для запуска сервера выполните следующую команду:" "\e[36m"
+# Інструкції для запуску сервера
+print_color_text "Для запуску сервера виконайте наступну команду:" "\e[36m"
 print_color_text "java -jar $HOME/$SERVER_DIR/server.jar" "\e[91m"
--- termux.sh
+++ termux.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-# Функция для вывода текста цветом
+# Функція для виведення тексту кольором
 print_color_text() {
     local text="$1"
     local color="$2"
     echo -e "${color}${text}\e[0m"
 }
 
-# Обновление системы
+# Оновлення системи
 clear
-print_color_text "Обновление системы..." "\e[36m"
+print_color_text "Оновлення системи..." "\e[36m"
 sleep 1
 clear
 apt update -y && apt upgrade -y
 sleep 1
 clear
 
-# Установка необходимых пакетов
-print_color_text "Установка необходимых пакетов..." "\e[36m"
+# Встановлення необхідних пакетів
+print_color_text "Встановлення необхідних пакетів..." "\e[36m"
 pkg install wget git proot proot-distro openssh -y
 sleep 1
 clear
 
-# Инструкции для установки сервера
-print_color_text "Для установки сервера нужно скачать дистрибьютив Linux" "\e[36m"
+# Інструкції для встановлення сервера
+print_color_text "Для встановлення сервера потрібно завантажити дистрибутив Linux" "\e[36m"
 sleep 1
-print_color_text "Для этого нуджно ввести команду: \e[91mproot-install distro\e[0m" "\e[36m"
+print_color_text "Для цього потрібно ввести команду: \e[91mproot-install distro\e[0m" "\e[36m"
 sleep 1
-print_color_text "Пример установки Ubuntu:" "\e[36m"
+print_color_text "Приклад встановлення Ubuntu:" "\e[36m"
 sleep 1
 print_color_text "\e[91mproot-distro install ubuntu\e[0m" "\e[91m"
 sleep 1
-
